50ee39c167c64d235bd467fd2e7b86c9fecfb854
kurt <qodydgus97@kakao.com>
2023-02-15 22:55:04 +0900

    AuthenticationProvider 추가

--- src/main/java/com/kurt/practice/springbootsecuritypractice/config/WebAuthorizationConfig.java
+++ src/main/java/com/kurt/practice/springbootsecuritypractice/config/WebAuthorizationConfig.java
@@ -2,6 +2,8 @@ package com.kurt.practice.springbootsecuritypractice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,4 +21,15 @@ public class WebAuthorizationConfig {
         return http.build();
     }
 
+    // authenticate 의 인증 메서드를 제공하는 매니저
+    @Bean
+    public AuthenticationManager authenticationManager () {
+        return new ProviderManager(customAuthenticationProvider());
+    }
+
+    // 인증 제공자. 구현에 따라 다르겠지만 username, password 요구하게 작성되어 있음.
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider(){
+        return new CustomAuthenticationProvider();
+    }
 }
