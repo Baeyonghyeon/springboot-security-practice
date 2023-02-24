45759e73bcd4845800ed4a8b88e51bd8be672480
kurt <qodydgus97@kakao.com>
2023-02-24 20:42:17 +0900

    비즈니스 논리 서버 구현
    비즈니스 논리 서버 구현

--- token-security-server-practice2/src/main/java/com/example/tokensecurityserverpractice2/authentication/config/SecurityConfig.java
+++ token-security-server-practice2/src/main/java/com/example/tokensecurityserverpractice2/authentication/config/SecurityConfig.java
@@ -5,7 +5,11 @@ import com.example.tokensecurityserverpractice2.authentication.filter.JwtAuthent
 import com.example.tokensecurityserverpractice2.authentication.provider.OtpAuthenticationProvider;
 import com.example.tokensecurityserverpractice2.authentication.provider.UsernamePasswordAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
@@ -16,5 +20,15 @@ public class SecurityConfig {
     private final OtpAuthenticationProvider otpAuthenticationProvider;
     private final UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider;
 
+    @Bean
+    protected AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth) {
+        return auth
+                .authenticationProvider(otpAuthenticationProvider)
+                .authenticationProvider(usernamePasswordAuthenticationProvider);
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
